c3577fb258ffb29a9bb53ecf278e7c1940bf9120
Игонин Павел <88630513+Pavel7334@users.noreply.github.com>
2023-04-28 18:56:51 +0400

    completed the task sequence

--- tasks/sequence.py
+++ tasks/sequence.py
@@ -16,3 +16,25 @@
     random.randint(-n, n)
 - метод print_seq, который выводит последовательность на экран
 """
+import random
+
+
+class RandSequence:
+
+    def __init__(self, n: int) -> None:
+        self.n = n
+        self.sequence = []
+        self.generate()
+
+    def generate(self, m=None) -> None:
+        n = m or self.n
+        self.sequence = [random.randint(-n, n) for _ in range(n)]
+
+    def print_seq(self) -> None:
+        print(f"Вот такая последовательность {self.sequence}")
+
+
+sequ = RandSequence(10)
+sequ.print_seq()
+sequ.generate()
+sequ.print_seq()
